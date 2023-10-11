1a6d490fa33fe6ac5cf8d935c1fae6adf1f73332
Gourab Saha <gourabsaha2062003@gmail.com>
2023-10-11 23:02:03 +0530

    format document

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ let voices = [];
 let voiceselect = document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)));
+  voices.forEach(
+    (voice, i) => (voiceselect.options[i] = new Option(voice.name, i))
+  );
 };
 
 voiceselect.addEventListener("change", () => {
-    speech.voice = voices[voiceselect.value];
-})
+  speech.voice = voices[voiceselect.value];
+});
 // selectbox end
 
 document.querySelector("button").addEventListener("click", () => {
-    speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
-})
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
